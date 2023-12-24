dd902b76d122d694acac3b8d93681cea7c073004
iamshiv007 <Github@#007>
2023-12-24 21:55:45 +0530

    📗project initilized with storybook

---
+++ src/components/Requirements/Requirements.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+export function Requirements(){
+    return (<div>Hello world</div>)
+}
---
+++ src/stories/Requirements.stories.js
@@ -0,0 +1,12 @@
+import { Requirements } from "../components/Requirements/Requirements"
+
+export default {
+    title:"Requirements",
+    component: Requirements
+}
+
+export const FirstStory = {
+    args: {
+        title: "hello"
+    }
+}
