a561236a7737115d132e3a93d731e7a80d90fc4f
iamshiv007 <Github@#007>
2023-12-25 11:51:14 +0530

    ✔️component created

--- src/components/Requirements/Requirements.js
+++ src/components/Requirements/Requirements.js
@@ -1,5 +1,26 @@
-import React from "react"
+import '../../styles.css';
+import { Requirement } from '../Requirement';
+import React, { useCallback, useEffect } from 'react';
 
-export function Requirements(){
-    return (<div>Hello world</div>)
-}
+export const Requirements = ({
+    value,
+    requirements,
+    onValidChange,
+  }) => {
+  const validChangeCb = useCallback(onValidChange, []);
+
+  useEffect(() => {
+    validChangeCb(
+      requirements.every(r => r.validator(value))
+    );
+  }, [value, requirements, validChangeCb]);
+
+  return requirements.map((r, index) => (
+    <Requirement
+      key={index}
+      value={value}
+      requirement={r}
+      onValidChange={onValidChange}
+    />
+  ));
+};
--- src/stories/Requirements.stories.js
+++ src/stories/Requirements.stories.js
@@ -1,8 +1,52 @@
+import React, { useState } from "react";
 import { Requirements } from "../components/Requirements/Requirements"
+import './styles.css';
+
+const MyFun = () => {
+    const [valid,  setValid] = useState(false);
+    const [password, setPassword] = useState('');
+    const [username, setUsername] = useState('');
+  
+    const passwordRequirements = [
+      {
+        text: 'Must be at least 8 characters',
+        validator: val => val.length >= 8,
+      },
+      {
+        text: 'Must contain at least one number',
+        validator: val => /\d/g.test(val),
+      },
+      {
+        text: 'Must contain at least one lower-case letter',
+        validator: val => /[a-z]/g.test(val),
+      },
+      {
+        text: 'Must contain at least one upper-case letter',
+        validator: val => /[A-Z]/g.test(val),
+      }
+    ];
+  
+    return (
+      <div className='form'>
+        <h1>Signup</h1>
+  
+        <Requirements
+          value={password}
+          requirements={passwordRequirements}
+          onValidChange={isValid => setValid(isValid)}
+        />
+  
+        <input placeholder='Username' value={username} onChange={e => setUsername(e.target.value)} />
+        <input placeholder='Password' type='password' value={password} onChange={e => setPassword(e.target.value)} />
+  
+        <button disabled={!valid || !username}>Sign Up</button>
+      </div>
+    );
+  }
 
 export default {
     title:"Requirements",
-    component: Requirements
+    component: MyFun
 }
 
 export const FirstStory = {
@@ -10,3 +54,4 @@ export const FirstStory = {
         title: "hello"
     }
 }
+
